b7e33f88b3df394121d917466299b08dcdc8f2dd
Marta Castrillo <mart.castrillo@gmail.com>
2023-05-16 11:26:27 +0200

    add recursividad

--- intermedio/recursividad.js
+++ intermedio/recursividad.js
@@ -1,6 +1,7 @@
  // PROBLEMA:
 // Deseamos imprimir una serie de números desde el 0 hasta n números. En este caso
 // hasta el 4
+
 let numerito = 0; // Declaramos desde el número que deseamos partir.
 
 // Usamos un bucle while para repetir este proceso hasta que se cumpla la condición:
@@ -8,7 +9,7 @@ while(numerito < 5) {
 	console.log(numerito);
 	numerito++;
 } 
-console.log("recursividad :");
+console.log("recursividad 1 :");
 // Función recursiva:
 function recursiva(numerito) { // Recive un número
 	console.log(numerito); // Imprimimos en consola el número
@@ -19,4 +20,23 @@ function recursiva(numerito) { // Recive un número
 		return 5;
 	}
 }
-recursiva(0)
+console.log("recursividad 2 :");
+const numeritos = [1,2,3,4,5,6,7,8,9,9,9,33];
+let numer = 0;
+for (let index = 0; index < numeritos.length; index++) {
+  
+   numer = numeritos[index];
+   console.log(numer);
+    
+}
+console.log("recursividad 3 :");
+const numeritos2 = [1,2,3,4,5,6,7,8,9,9,9,33];
+function recursiva2(numerito) { 
+    if(numerito.length != 0){
+        const firstNum = numerito[0];
+        console.log(firstNum);
+        numerito.shift();
+        return recursiva2 (numerito);
+    }
+}
+recursiva2(numeritos2);
