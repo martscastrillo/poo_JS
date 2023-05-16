099d1f9436967b5eb57afa1c2860f87642e282d8
Marta Castrillo <mart.castrillo@gmail.com>
2023-05-16 10:42:45 +0200

    add object.defineProperty

--- intermedio/main.js
+++ intermedio/main.js
@@ -8,9 +8,43 @@ const juan = {
     this.approvedCourses.push(newCourse);
   }
 };
+Object.defineProperty(juan, "nombreNuevaPropiedad", {
+	value: "JavaScript", // Valor que tendrá
+	enumerable: false,
+	writable: true,
+	configurable: false,
+});
+
+Object.defineProperty(juan, "navigator", { // Creamos un nuevo atributo
+	value: "Chrome",
+	enumerable: false, // 👀
+	writable: true,
+	configurable: true,
+});
+Object.defineProperty(juan, "terminal", { // Creamos un nuevo atributo
+	value: "WSL",
+	enumerable: true,
+	writable: true,
+	configurable: false, // 👀
+});
+
+console.log( // Imprimimos las propiedades del objeto
+	Object.getOwnPropertyNames(juan)
+); // [ 'name', 'age', 'approvedCourses', 'addCourse', 'navigator' ] 👈 Ya nos aparece
+
+
 
 //console.log(Object.keys(juan));
 // console.log(Object.getOwnPropertyNames(juan));
 // console.log(Object.entries(juan));
 
-console.log(Object.getOwnPropertyDescriptors(juan));
+// console.log(Object.getOwnPropertyDescriptors(juan));
+console.log( // Mostramos las propiedades del objeto previamente... 👁👁
+	Object.keys(juan)
+); // [ 'name', 'age', 'approvedCourses', 'addCourse', 'terminal' ]
+
+delete terminal; // Intentamos eliminar ❌
+
+console.log( // Listamos los atributos para comprobar si se eliminó `terminal` 🤔
+	Object.keys(juan)
+); // [ 'name', 'age', 'approvedCourses', 'addCourse', 'terminal' ] 👈 NO se eliminó
