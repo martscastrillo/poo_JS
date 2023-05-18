a9d61e7c6795260f21a6bfe8ceca707e031c5e48
Marta Castrillo <mart.castrillo@gmail.com>
2023-05-18 16:00:14 +0200

    add duck type

--- intermedio/ducktyping.js
+++ intermedio/ducktyping.js
@@ -1,90 +1,106 @@
-// El duck typing es la forma de programar donde identificamos los elementos por los métodos y atributos que tenga por dentro.
-
 function isObject(subject) {
-    return typeof subject == "object";
-  }
-  
-  function isArray(subject) {
-    return Array.isArray(subject);
-  }
-  
-  function requiredParam(param) {
-    throw new Error(param + " es obligatorio");
-  }
-  function createLearningPath({ name = requiredParam("name"),courses = []}) {
-    const private = { 
+  return typeof subject == "object";
+}
 
-        "_name": name,
-        "_courses": courses,
-      };
-      const public = {
-        get name(){
-            return private["_name"];
-        },
-       set name(newName){
-        if(newName.lenght != 0){
-            private["_name"] = newName;
-        }else{
-            console.warn("Tu nombre debe tener al menos un carcter");
-        }
-           
-        },
-        get courses(){
-            return private["_courses"];
-        },
-       
-      }
-  }
-  function createStudent({
-    name = requiredParam("name"),
-    email = requiredParam("email"),
-    age,
-    twitter,
-    instagram,
-    facebook,
-    approvedCourses = [],
-    learningPaths = [],
-  } = {}) {
-    const privateAtributos = { // 👈👈
+function isArray(subject) {
+  return Array.isArray(subject);
+}
 
-      "_name": name,
-    };
+function requiredParam(param) {
+  throw new Error(param + " es obligatorio");
+}
 
-  
-    const publicAtributos = {
-		// El resto de atributos serán públicos:
+function createLearningPath({
+	name = requiredParam("name"), // Campo es obligatorio
+	courses = [], // Lista de Cursos que pertencen a la ruta de aprendizaje
+}) {
+	const private = { // Atributos privados
+		"_name": name,
+		"_courses": courses,
+	};
+	const public = { // Getters y Setters
+		get name() {
+			return private["_name"];
+		},
+		set name(newName) {
+			if (newName.length != 0) {
+				private["_name"] = newName;
+			} else {
+				console.warn("El nombre debe tener al menos 1 caracter");
+			}
+		},
+		get courses() {
+			return private["_courses"];
+		},
+	};
+}
+
+function createStudent({
+  name = requiredParam("name"),
+  email = requiredParam("email"),
+  age,
+  twitter,
+  instagram,
+  facebook,
+  approvedCourses = [],
+  learningPaths = [],
+} = {}) {
+  const privateAtributos = {
+    "_name": name,
+		"_learningPaths": learningPaths, // 👈👈
+  };
+
+  const publicAtributos = {
     email,
     age,
     approvedCourses,
-    learningPaths,
+    //learningPaths,👈👈
     socialMedia: {
       twitter,
       instagram,
       facebook,
     },
-    get name(){
-        return privateAtributos["_name"];
+		get name() {
+      return privateAtributos["_name"];
     },
-   set name(newName){
-    if(newName.lenght != 0){
+		set name(newName) {
+      if (newName.length != 0) {
         privateAtributos["_name"] = newName;
-    }else{
-        console.warn("Tu nombre debe tener al menos un carcter");
-    }
-       
-    }
-  }
+      } else {
+        console.warn("Tu nombre debe tener al menos 1 caracter");
+      }
+    },
+		get learningPaths() { // 👈👈
+			return private["__learningPaths"];
+		},
+		set learningPaths(newLP) { // 👈👈
+			// AQUÍ empezamos a aplicar DUCK TYPING 👀🦆
+      if (!newLP.name) {
+				// Si la nueva ruta de aprendizaje NO tiene el atributo "name"...
+        console.warn("Tu LP no tiene la propiedad name");
+        return; // Cortamos el proceso y no agregamos la ruta de aprendizaje
+      }
 
-    return publicAtributos ;
-  }
+      if (!newLP.courses) {
+				// Si la nueva ruta NO tiene asignado un array
+				// en el atributo "courses"
+        console.warn("Tu LP no tiene courses");
+        return; // Cortamos el proceso y no agregamos la ruta de aprendizaje
+      }
 
-  // Creamos un nuevo objeto
-const juan = createStudent({ email: "juanito@frijoles.co", name: "Juanito" });
+      if (!isArray(newLP.courses)) {
+				// Si el atributo "courses" en la nueva ruta de aprendizaje NO es un Array
+        console.warn("Tu LP no es una lista (*de cursos)");
+        return; // Cortamos el proceso y no agregamos la ruta de aprendizaje
+      }
 
-// Intentamos eliminar y alterar los métodos changeName y readName
+			// Si la nueva ruta de aprendizaje pasó por todas las validaciones
+			// correctamente...Quiere decir que SÍ es una ruta válida tal como
+			// la deseamos que fuese. Por tanto, procedemos a añadir ese Learning Path
+			// a la lista de rutas del estudiante:
+      private["_learningPaths"].push(newLP);
+    },
+  };
 
-
-console.log(juan.name); // Se ejecuta el GETTER
-juan.name = "Rigoberto"; // Se ejecuta el SETTER
-console.log(juan.name);
-console.log(Object.getOwnPropertyDescriptors(juan));
+  return publicAtributos;
+}
